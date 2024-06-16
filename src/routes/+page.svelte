1a4ec9ce602edb879afb9fef4cd6316bafa1096d
Mike Peiman <mikepeiman@gmail.com>
2024-06-16 08:50:38 -0400

    added logo bg as watermark

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,39 @@
 <script>
 </script>
 
-<section class="w-full h-full">
-	<div class="flex flex-col h-full w-full justify-center items-center">
-		<h1
-			class="-mt-[20vh] mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
-		>
-			You need a website.
-		</h1>
-		<h1
-			class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
-		>
-			We build websites.
-		</h1>
-		<h1
-			class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
-		>
-			Let's talk.
-		</h1>
+<section class="w-full h-full relative ">
+	<div class="hero-bg w-full h-full">
 	</div>
+		<div class="flex flex-col h-full w-full justify-center items-center">
+			<h1
+				class="-mt-[20vh] mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+			>
+				You need a website.
+			</h1>
+			<h1
+				class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+			>
+				We build websites.
+			</h1>
+			<h1
+				class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+			>
+				Let's talk.
+			</h1>
+		</div>
+
 </section>
+
+<style>
+	.hero-bg {
+		background-image: url('/images/logo.svg');
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+		opacity: 0.1;
+		position: absolute;
+		top: 0;
+		left: 0;
+		/* min-height: 100vh; */
+	}
+</style>
