e0b4449a5f4461b6a23eb2fad0f1936841272640
Mike Peiman <mikepeiman@gmail.com>
2024-06-16 11:02:28 -0400

    added CTA section in hero

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,52 @@
+<script>
+	import { Button, GradientButton } from 'flowbite-svelte';
+	import { IconArrowRight, IconCalendar } from '@tabler/icons-svelte';
+</script>
+
+<div class="hero-bg w-full h-full"></div>
+<div class="flex flex-col h-full w-full justify-center items-center font-display">
+	<h1
+		class="-mt-[20vh] mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+	>
+		You need a website?
+	</h1>
+	<h1
+		class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+	>
+		We build websites.
+	</h1>
+	<h1
+		class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+	>
+		Let's talk.
+	</h1>
+
+	<div class="subtext text-black dark:text-white font-inter text-xl text-center mt-8 w-3/4">
+		<p>
+			You've got enough to do. Let us handle this part.
+		</p>
+		<p class="mt-1">Quick, easy, and stress-free. That's our process.</p>
+	</div>
+	<div class="cta-section my-8">
+		<GradientButton href="/contact" color="greenToBlue" size="lg" class="mr-4"
+			>Get a Quick Quote <IconArrowRight class="ml-2" /></GradientButton
+		>
+		<GradientButton href="/contact" color="purpleToPink" size="lg"
+			>Book a Call <IconCalendar class="ml-2" /></GradientButton
+		>
+	</div>
+</div>
+
+<style>
+	.hero-bg {
+		background-image: url('/images/logo.svg');
+		background-position: center;
+		background-repeat: no-repeat;
+		background-size: cover;
+		opacity: 0.1;
+		position: absolute;
+		top: 0;
+		left: 0;
+		/* min-height: 100vh; */
+	}
+</style>
