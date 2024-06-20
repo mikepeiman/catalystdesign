1c5d43e9bf5d3a9bc546f42fd97dbd2f70c7804e
Mike Peiman <mikepeiman@gmail.com>
2024-06-20 12:43:43 -0400

    blog section post improvements

--- src/lib/components/BlogSection.svelte
+++ src/lib/components/BlogSection.svelte
@@ -46,7 +46,7 @@
 
     <div class="max-w-7xl mx-auto px-4 pt-16 pb-20">
   
-      <h2 class="text-3xl font-bold text-gray-900">Latest Blog Posts</h2>
+      <h2 class="text-3xl font-bold  ">Latest Blog Posts</h2>
   
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
   
@@ -54,56 +54,59 @@
   
           <article class="bg-zinc-600 text-white p-6 rounded-lg shadow">
   
-            <div class="flex items-center justify-between">
-              <div class="flex items-center">
+            <div class="flex items-center justify-between p-0 mb-4">
+              <div class="flex items-center text-zinc-300  bg-zinc-800 rounded-lg p-1 pr-2 mx-1 text-nowrap ">
                 <svelte:component this={post.icon} class="w-6 h-6 text-blue-500 " />
-                <p class="ml-2 text-sm font-medium text-gray-900">
+                <p class="ml-2 text-sm font-medium  ">
                   {post.type}
                 </p>
               </div>
-  
-              <div class="flex items-center">
-                {#each post.tags as tag}
-                  <p class="text-sm font-medium text-gray-900">
-                    {tag}
-                  </p>
-                {/each}
-              </div>
+              <time class="block mt-1 text-sm text-zinc-300">
+                {post.date}
+              </time> 
+
             </div>
             <header>
-              <h3 class="text-lg font-medium text-gray-900">
+              <h3 class="text-xl font-bold  ">
                 {post.title}
               </h3>
   
-              <time class="block mt-1 text-sm text-gray-600">
-                {post.date}
-              </time> 
+
             </header>
   
-            <p class="mt-4 text-gray-600">
+            <p class="mt-4 text-white">
               {post.preview}
             </p>
   
-            <footer class="flex items-center justify-between mt-6">
+            <footer class="flex items-center justify-between mt-6 text-zinc-300">
   
-              <div class="flex items-center">
-                <img 
-                  class="w-8 h-8 rounded-full mr-3" 
-                  src={post.author.img} 
-                >
-  
-                <p class="text-sm font-medium text-gray-900">
-                  {post.author.name}
-                </p>
+              <div class="flex flex-col w-full">
+                  <div class="flex items-center justify-between">
+                      <div class="flex items-center">
+                        <img
+                          class="w-8 h-8 rounded-full mr-3"
+                          src={post.author.img}
+                        >
+                        <p class="text-sm font-medium text-white">
+                          {post.author.name}
+                        </p>
+                      </div>
+                      <a
+                        href="/blog"
+                        class="flex items-center text-zinc-300 hover:underline"
+                      >
+                        Read More
+                        <IconChevronRight class="ml-1" />
+                      </a>
+                  </div>
+                  <div class="flex items-start justify-start flex-wrap mt-4">
+                    {#each post.tags as tag}
+                      <p class="text-sm font-medium text-zinc-300 bg-zinc-800 rounded-lg px-1 mx-1 text-nowrap">
+                        {tag}
+                      </p>
+                    {/each}
+                  </div>
               </div>
-  
-              <a 
-                href="/blog"
-                class="flex items-center text-blue-600 hover:underline"
-              >
-                Read More
-                <IconChevronRight class="ml-1" />
-              </a>
   
             </footer>
   
