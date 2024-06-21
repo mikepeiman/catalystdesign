5a320c07cc5ca85e2e8e10f5b6b708ea7c7f95ce
Mike Peiman <mikepeiman@gmail.com>
2024-06-21 18:32:18 -0400

    working on footer layout

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,61 +1,85 @@
 <script>
 	import {
-		IconBrandTwitter,
+		IconBrandLinkedin,
 		IconBrandYoutube,
 		IconBrandFacebook,
-		IconBrandInstagram
+		IconBrandInstagram,
+    IconMap
 	} from '@tabler/icons-svelte';
-	import { IconMap } from '@tabler/icons-svelte';
+
+let footerLinks = [
+  {
+    name: 'Home',
+    href: '#'
+  },
+  {
+    name: 'About',
+    href: '#'
+  },
+  {
+    name: 'Services',
+    href: '#'
+  },
+  {
+    name: 'Contact',
+    href: '#'
+  },
+  {
+    name: 'Blog',
+    href: '#'
+  },
+  {
+    name: 'Portfolio',
+    href: '#'
+  }
+]
+
 </script>
 
 <footer class="bg-gray-100 dark:bg-zinc-900 text-zinc-900 dark:text-white">
 	<div class="w-full mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
-		<nav class="-mx-5 -my-2 flex flex-wrap justify-around justify-center" aria-label="Footer">
+		<nav class="-mx-5 -my-2 flex justify-between" aria-label="Footer">
 			<!-- Left section -->
-      <!-- <IconMap class="w-5 h-5 mr-2 text-gray-400" /> -->
-			<div class="flex">
-        <div class="flex flex-col items-start justify-start">
-                <div class="px-5 py-2">
-                  <div class="flex items-center justify-center">
-                    <a href="#" class="flex items-center">
-                      <img src="images/logo-blue.svg" alt="Company logo" class="h-8 mr-3" />
-                      <span class="self-center text-xl font-semibold"> Catalyst Design & Development </span>
-                    </a>
-                  </div>
-                  <address class="flex items-center mt-4">
-                    216 Grovehill Cres<br> Kitchener, ON, Canada<br> N2R0K9
-                  </address>
-                </div>
-              </div>
-        <!-- Links -->
-        <div class="px-5 py-2">
-          <p class="text-sm text-gray-500">
-            <a href="#services" class="hover:text-gray-600">Services</a>
-          </p>
-          <p class="text-sm text-gray-500">
-            <a href="#work" class="hover:text-gray-600">Work</a>
-          </p>
-          <p class="text-sm text-gray-500">
-            <a href="#team" class="hover:text-gray-600">Team</a>
-          </p>
-          <p class="text-sm text-gray-500">
-            <a href="#contact" class="hover:text-gray-600">Contact</a>
-          </p>
+			<!-- <IconMap class="w-5 h-5 mr-2 text-gray-400" /> -->
+			<div class="flex justify-between items-start">
+				<div class="flex flex-col items-start justify-start">
+					<div class="px-5 py-2">
+						<div class="flex items-center justify-center">
+							<a href="#" class="flex items-center">
+								<img src="images/logo-blue.svg" alt="Company logo" class="h-8 mr-3" />
+								<span class="self-center text-xl font-semibold">
+									Catalyst Design & Development
+								</span>
+							</a>
+						</div>
+						<address class="flex items-center mt-4">
+							216 Grovehill Cres<br /> Kitchener, ON, Canada<br /> N2R0K9
+						</address>
+					</div>
+				</div>
+				<!-- Links -->
         </div>
-      </div>
+        
+        <!-- Social icons -->
+        <div class="flex justify-center self-center items-start px-5 py-2">
+        <div class="px-12 text-base text-zinc-300 grid grid-cols-2 gap-x-8">
+          {#each footerLinks as link}
+            <p>
+              <a href={link.href} class="hover:text-cyan-400">{link.name}</a>
+            </p>
+          {/each}
 
-			<!-- Social icons -->
-			<div class="flex justify-center px-5 py-2">
+        </div>
 				<a href="#" class="text-gray-400 hover:text-gray-500">
 					<IconBrandFacebook class="w-5 h-5" />
 				</a>
 
 				<a href="#" class="text-gray-400 hover:text-gray-500 ml-3">
-					<IconBrandFacebook class="w-5 h-5" />
+					<IconBrandInstagram class="w-5 h-5" />
 				</a>
 
 				<a href="#" class="text-gray-400 hover:text-gray-500 ml-3">
-					<IconBrandFacebook class="w-5 h-5" />
+					<IconBrandLinkedin class="w-5 h-5" />
 				</a>
 			</div>
 		</nav>
@@ -64,7 +88,6 @@
 			<p class="text-sm text-gray-500">
 				&copy; {new Date().getFullYear()} Catalyst Web Design. All rights reserved.
 			</p>
-
 		</div>
 	</div>
 </footer>
