2df13b7fb6809f3b7b99fa87189660c88fa70b6e
Mike Peiman <mikepeiman@gmail.com>
2024-06-22 07:24:27 -0400

    removed flowbite files and imports, tweaked section borders

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,6 @@
 	import Hero from '$lib/components/Hero.svelte';
 	import SocialProof from '$lib/components/SocialProof.svelte';
 	import PricingTable from '$lib/components/PricingTable.svelte';
-	import PricingTableFB from '$lib/components/flowbite/PricingTableFB.svelte';
-	import BlogSectionFB from '$lib/components/flowbite/BlogSection.svelte';
 	import BlogSection from '$lib/components/BlogSection.svelte';
 </script>
 
